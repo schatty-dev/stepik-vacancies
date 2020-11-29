489e6f7c223cf8baa0364359fd9bbcbcae31a571
Igor Kuznetsov <igorkuznetsov14@gmail.com>
2020-11-29 16:46:40 +0300

    Home page picks data

--- vacancies/views.py
+++ vacancies/views.py
@@ -1,10 +1,30 @@
+from collections import Counter
 from django.shortcuts import render
 
-# Create your views here.
+from vacancies.models import Company, Vacancy, Speciality
+
+
+def get_vacancy_preview_info():
+    specs = [vac.specialty.title for vac in Vacancy.objects.all()]
+    specs_cnt = dict(Counter(specs))
+    return {"specialties": specs_cnt}
+
+
+def get_company_preview_info():
+    companies = [vac.company.name for vac in Vacancy.objects.all()]
+    company_cnt = Counter(companies)
+    company_logos = {}
+    for company in Company.objects.all():
+        num = company_cnt[company.name]
+        if num:
+            company_logos[company.logo] = num
+    return {"companies": company_logos}
 
 
 def main_view(request):
-    context_data = {}
+    context_data = {**{},
+                    **get_company_preview_info(),
+                    **get_vacancy_preview_info()}
     return render(request, "vacancies/index.html", context=context_data)
 
 
